9d593c2949e864fe87ea617302cbb987b9790e72
huanyin <2450421960@qq.com>
2020-03-04 17:17:06 +0800

    init a repo

---
+++ src/main/java/com/yun/work/community/CommunityApplication.java
@@ -0,0 +1,13 @@
+package com.yun.work.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
